cc1d4cc709c2d75e31c57dbe1fcdf1d1753db960
Vongo <adelmarre@vongo.eu>
2014-08-14 15:13:23 +0200

    use only top keys

--- xnxx.R
+++ xnxx.R
@@ -24,30 +24,48 @@ data <- read.csv("./xnxx.csv", sep = ",", header = T, stringsAsFactors = F)
 groups <- sapply(data$tags,spleat)
 keys <- unlist(groups)
 names(keys) <- c()
-keys <- unique(keys)
 
-n <- length(keys)
+occurrences <- data.frame(matrix(vector(), 0, 2, dimnames=list(c(), c("Key","Count"))), stringsAsFactors=F)
+io <- 1
+pb <- txtProgressBar(1,length(unique(keys)),1,style=3)
+for (k in unique(keys)) {
+	occurrences[io,] <- c(k,sum(grepl(k,keys))) # Here
+	setTxtProgressBar(pb,io)
+	io <- io + 1
+}
+close(pb)
+occurrences <- occurrences[order(occurrences$Count),]
+top <- occurrences[1:10000,]
+
+uKeys <- unique(keys)
+
+n <- nrow(top)
 # amtx <- matrix(0,nrow=n,ncol=n,dimnames=list(keys,keys))
 amtx <- matrix(0,nrow=n,ncol=n)
-colnames(amtx) <- keys
-rownames(amtx) <- keys
+colnames(amtx) <- top[,"Key"]
+rownames(amtx) <- top[,"Key"]
 
-
-
+pb <- txtProgressBar(1,length(groups),1,style=3)
+ng <- 1
 for (group in groups) {
 	sequ <- 1:length(group)
+	setTxtProgressBar(pb,ng)
 	for (i in sequ) {
 		term <- group[i]
 		if (length(term)>0)
 			if (!is.na(term))
-				for (o in sequ[which(sequ!=i)]) {
-					other <- group[o]
-					if (length(other)>0)
-						if (!is.na(other))
-							amtx[term,other] <- amtx[term,other]+1
-				}
+				if (term %in% top$Key)
+					for (o in sequ[which(sequ>i)]) {
+						other <- group[o]
+						if (length(other)>0)
+							if (!is.na(other))
+								if (term %in% top$Key)
+									amtx[term,other] <- amtx[term,other]+1
+					}
 	}
+	ng <- ng + 1
 }
+close(pb)
 
 plot.success <- function(x) qplot(x=colnames(sucksess),y=sucksess[x,],stat="identity",geom="histogram",ylab = "Views per video", xlab = "Year", main = "Evolution of popularity over time")
 
